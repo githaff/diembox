d1d39c44369f27aae7b176d0f9e91dee101092f8
Dmitry Lavnikevich <haff@midgard.by>
2013-11-16 18:49:49 +0300

    Added usage/help output for basic ltools executed as ltools itself (not through a link).

--- main.c
+++ main.c
@@ -6,6 +6,24 @@
 #include "tobin.h"
 
 
+char ltools_name[] = "ltools";
+int ltools_name_len = sizeof(ltools_name);
+void ltools_usage(void)
+{
+	printf("Usage: ltools [OPTIONS] COMMAND [COMMAND_OPTIONS]\n"
+		   "Embedded developer toolbox.\n"
+           "Targeted mainly at aiding embedded low-level programming (but of course\n"
+           "can be used by anyone) and implements basic useful tools which perfectly\n"
+           "may be replaced by other different tools. But it is simply convenient\n"
+           "to hold it all in one console utility.\n"
+		   "\n"
+		   "Options:\n"
+		   "  -h|--help   Print this help\n"
+           "\n"
+           "Commands:\n");
+}
+
+
 int main(int argc, char *argv[])
 {
 	int ret = 0;
@@ -13,7 +31,11 @@ int main(int argc, char *argv[])
 
 	toolname = basename(argv[0]);
 
-	if (!strncmp(toolname, tobin_name, tobin_name_len))
+	if (!strncmp(toolname, ltools_name, ltools_name_len)) {
+        ltools_usage();
+		return ret;
+    }
+	else if (!strncmp(toolname, tobin_name, tobin_name_len))
 		ret = do_tobin(argc, argv);
 	else {
 		printf("Error: unknown toolname '%s'\n", toolname);
