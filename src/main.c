bf60ae0702c08bea77cbbfcdfddbd7a92567696e
Dmitry Lavnikevich <haff@midgard.by>
2014-05-29 00:23:45 +0300

    Parsing command line options with extopts.
    Module name presented by:
    - name of executable which can be in format:
      <module_name>
      embox.<module_name>
    - first command line argument if executable name is 'embox'

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+#include <libgen.h>
 #include <extopts/extopts.h>
+#include <extopts/extmods.h>
 
 
 struct extopt opts[];
@@ -28,12 +31,41 @@ struct extopt opts[] = {
 
 int main(int argc, char *argv[])
 {
-	if (extopts_get(&argc, argv, opts))
-		return 1;
-	if (opts_help)
-		embox_usage();
-	else
-		printf("U-u-u-u\n");
+	int ret = 0;
+	struct extmod *module = 0;
+	char *execname;
 
-	return 0;
+	execname = basename(argv[0]);
+
+	if (!strncmp(execname, embox_name, embox_name_len)) {
+		if (argc > 1)
+			module = extmod_find(argv[1]);
+	}
+	else {
+		module = extmod_find(execname);
+		if (!module) {
+			char buf[1024];
+			sprintf(buf, "%s.%s", embox_name, execname);
+			module = extmod_find(buf);
+		}
+	}
+
+	if (module)
+		ret = extmod_exec(argc, argv, module);
+	else {
+		ret = extopts_get(&argc, argv, opts);
+		if (ret)
+			goto err;
+
+		if (opts_help) {
+			embox_usage(opts);
+			goto end;
+		}
+	}
+
+end:
+	return ret;
+
+err:
+	return ret;
 }
