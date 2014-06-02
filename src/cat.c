c94982a2adc2858a2e19dc399972d1b6b2c2e413
Dmitry Lavnikevich <haff@midgard.by>
2014-06-02 23:19:40 +0300

    Further cat simplification

--- src/cat.c
+++ src/cat.c
@@ -3,35 +3,13 @@
 #include <extopts/extmods.h>
 
 
-int opts_help;
-
-struct extopt cat_opts[] = {
-	EXTOPTS_HELP(&opts_help),
-	EXTOPTS_END
-};
-
-void cat_help(void)
-{
-	puts("Cat doesn't need your help. He is purfectly fine.");
-}
-
 int cat_main(int argc, char *argv[])
 {
-	int ret = 0;
-
-	ret = extopts_get(&argc, argv, cat_opts);
-	if (opts_help) {
-		cat_help();
-		goto end;
-	}
-
 	puts(":3");
 
-end:
-	return ret;
+	return 0;
 }
 
-EXTMOD_DECL(cat, cat_main, cat_opts,
+EXTMOD_DECL(cat, cat_main, NULL,
 			"Cat",
-			"Usage: embox-cat [OPTION]...\n"
-			"Embedded developer's cat.")
+			"Cat doesn't need your help. He is purfectly fine.")
