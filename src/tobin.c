3207523e3ad4ce99b4c4837991203a2fbb4a33f4
Dmitry Lavnikevich <d.lavnikevich@sam-solutions.com>
2014-06-03 17:30:02 +0300

    Implemented integer types printing.

--- src/tobin.c
+++ src/tobin.c
@@ -30,44 +30,66 @@ int eval(char *exp)
 	return strtol(exp, NULL, 0);
 }
 
-void print_output(int val)
+char *byte_str(u8 byte)
 {
-	printf("31     24  23     16\n"
-		   "%d%d%d%d %d%d%d%d  %d%d%d%d %d%d%d%d\n"
-		   "15      8  7       0\n"
-		   "%d%d%d%d %d%d%d%d  %d%d%d%d %d%d%d%d\n",
-		   (val >> 31) & 0x1,
-		   (val >> 30) & 0x1,
-		   (val >> 29) & 0x1,
-		   (val >> 28) & 0x1,
-		   (val >> 27) & 0x1,
-		   (val >> 26) & 0x1,
-		   (val >> 25) & 0x1,
-		   (val >> 24) & 0x1,
-		   (val >> 23) & 0x1,
-		   (val >> 22) & 0x1,
-		   (val >> 21) & 0x1,
-		   (val >> 20) & 0x1,
-		   (val >> 19) & 0x1,
-		   (val >> 18) & 0x1,
-		   (val >> 17) & 0x1,
-		   (val >> 16) & 0x1,
-		   (val >> 15) & 0x1,
-		   (val >> 14) & 0x1,
-		   (val >> 13) & 0x1,
-		   (val >> 12) & 0x1,
-		   (val >> 11) & 0x1,
-		   (val >> 10) & 0x1,
-		   (val >> 9) & 0x1,
-		   (val >> 8) & 0x1,
-		   (val >> 7) & 0x1,
-		   (val >> 6) & 0x1,
-		   (val >> 5) & 0x1,
-		   (val >> 4) & 0x1,
-		   (val >> 3) & 0x1,
-		   (val >> 2) & 0x1,
-		   (val >> 1) & 0x1,
-		   val & 0x1);
+	static char str[] = "0000 0000";
+
+	str[0] = (byte >> 7) & 0x1 ? '1' : '0';
+	str[1] = (byte >> 6) & 0x1 ? '1' : '0';
+	str[2] = (byte >> 5) & 0x1 ? '1' : '0';
+	str[3] = (byte >> 4) & 0x1 ? '1' : '0';
+	str[5] = (byte >> 3) & 0x1 ? '1' : '0';
+	str[6] = (byte >> 2) & 0x1 ? '1' : '0';
+	str[7] = (byte >> 1) & 0x1 ? '1' : '0';
+	str[8] = byte & 0x1 ? '1' : '0';
+
+	return str;
+}
+
+void print_8(int val)
+{
+	puts("7       0");
+	printf("%s\n", byte_str(val));
+}
+
+void print_16(int val)
+{
+	u8 *bytes = (u8*)&val;
+
+	puts("15      8  7       0");
+	printf("%s  ", byte_str(bytes[1]));
+	printf("%s\n", byte_str(bytes[0]));
+}
+
+void print_32(int val)
+{
+	printf("val=%d\n", val);
+	u8 *bytes = (u8*)&val;
+
+	puts("31     24  23     16");
+	printf("%s  ", byte_str(bytes[3]));
+	printf("%s\n", byte_str(bytes[2]));
+	puts("15      8  7       0");
+	printf("%s  ", byte_str(bytes[1]));
+	printf("%s\n", byte_str(bytes[0]));
+}
+
+void print_64(int val)
+{
+	u8 *bytes = (u8*)&val;
+
+	puts("63     56  55     48");
+	printf("%s  ", byte_str(bytes[7]));
+	printf("%s\n", byte_str(bytes[6]));
+	puts("47      40  39       32");
+	printf("%s  ", byte_str(bytes[5]));
+	printf("%s\n", byte_str(bytes[4]));
+	puts("31     24  23     16");
+	printf("%s  ", byte_str(bytes[3]));
+	printf("%s\n", byte_str(bytes[2]));
+	puts("15      8  7       0");
+	printf("%s  ", byte_str(bytes[1]));
+	printf("%s\n", byte_str(bytes[0]));
 }
 
 int tobin_main(int argc, char *argv[])
@@ -75,6 +97,9 @@ int tobin_main(int argc, char *argv[])
 	int ret = 0;
 	int result;
 	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 
 	ret = extopts_get(&argc, argv, tobin_opts);
 	if (opts_help) {
@@ -84,9 +109,9 @@ int tobin_main(int argc, char *argv[])
 
 	for (i = 0; i < argc; i++) {
 		result = eval(argv[i]);
+		print_32(result);
 		if (i > 0)
 			printf("\n");
-		print_output(result);
 	}
 
 end:
