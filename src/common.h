969c70f611220131b681f94a0020bde5bfa4043f
Dmitry Lavnikevich <d.lavnikevich@sam-solutions.com>
2014-06-03 14:56:11 +0300

    Introduced pack of integer types for use in tobin.

--- src/common.h
+++ src/common.h
@@ -2,6 +2,18 @@
 #define __COMMON_H
 
 
+#include <stdint.h>
+
+
+typedef int8_t   s8;
+typedef uint8_t  u8;
+typedef int16_t  s16;
+typedef uint16_t u16;
+typedef int32_t  s32;
+typedef uint32_t u32;
+typedef int64_t  s64;
+typedef uint64_t u64;
+
 #define ARRAY_SIZE(ARRAY) (sizeof(ARRAY)/sizeof((ARRAY)[0]))
 
 
