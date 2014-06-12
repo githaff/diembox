930b3c66d74326ace5d199ca7886e18d3464ef0d
Dmitry Lavnikevich <d.lavnikevich@sam-solutions.com>
2014-06-12 18:58:49 +0300

    Modified operators calculation.

--- src/tobin-calc.c
+++ src/tobin-calc.c
@@ -415,6 +415,18 @@ struct symbol *symbol_clone(struct symbol *s)
 	return s_new;
 }
 
+#define S_INT(S)								\
+	((S->val.type == S8)  ? S->val.s8  :		\
+	 (S->val.type == U8)  ? S->val.u8  :		\
+	 (S->val.type == S16) ? S->val.s16 :		\
+	 (S->val.type == U16) ? S->val.u16 :		\
+	 (S->val.type == S32) ? S->val.s32 :		\
+	 (S->val.type == U32) ? S->val.u32 :		\
+	 (S->val.type == S64) ? S->val.s64 :		\
+	 (S->val.type == U64) ? S->val.u64 : 0)
+#define S_OP(S, S1, S2, OP)				\
+	S->val.s64 = ((S_INT(S1)) OP (S_INT(S2)))
+
 int rpn_eval(struct symbol_queue *rpn)
 {
 	struct symbol *s, *s_new;
@@ -443,44 +455,21 @@ int rpn_eval(struct symbol_queue *rpn)
 				exit(1);
 			}
 
+			s1->type = INTVAL;
 			switch (s->op) {
-			case MULT :
-				val = s1->val.s32 * s2->val.s32;
-				break;
-			case DIV :
-				val = s1->val.s32 / s2->val.s32;
-				break;
-			case REST :
-				val = s1->val.s32 % s2->val.s32;
-				break;
-			case PLUS :
-				val = s1->val.s32 + s2->val.s32;
-				break;
-			case MINUS :
-				val = s1->val.s32 - s2->val.s32;
-				break;
-			case SHIFT_L :
-				val = s1->val.s32 << s2->val.s32;
-				break;
-			case SHIFT_R :
-				val = s1->val.s32 >> s2->val.s32;
-				break;
-			case AND :
-				val = s1->val.s32 & s2->val.s32;
-				break;
-			case XOR :
-				val = s1->val.s32 ^ s2->val.s32;
-				break;
-			case OR :
-				val = s1->val.s32 | s2->val.s32;
-				break;
-			default :
-				val = -1;
-				break;
+			case MULT    : S_OP(s1, s1, s2, *);  break;
+			case DIV     : S_OP(s1, s1, s2, /);  break;
+			case REST    : S_OP(s1, s1, s2, %);  break;
+			case PLUS    : S_OP(s1, s1, s2, +);  break;
+			case MINUS   : S_OP(s1, s1, s2, -);  break;
+			case SHIFT_L : S_OP(s1, s1, s2, <<); break;
+			case SHIFT_R : S_OP(s1, s1, s2, >>); break;
+			case AND     : S_OP(s1, s1, s2, &);  break;
+			case XOR     : S_OP(s1, s1, s2, ^);  break;
+			case OR      : S_OP(s1, s1, s2, |);  break;
+			default      : val = -1;             break;
 			}
 
-			s1->type = INTVAL;
-			s1->val.s32 = val;
 			symbol_stack_push(stack, s1);
 		}
 
