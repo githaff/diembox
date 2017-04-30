0ac41526919de8d679402c78022330df5b3e46bb
Dmitry Lavnikevich <dmitry.lavnikevich@edality.by>
2017-04-30 20:45:39 +0300

    Update cmake get version extraction module to x.x.x format
    Taken from the extopts library.

--- cmake/git-version.cmake
+++ cmake/git-version.cmake
@@ -1,53 +1,76 @@
 ### Version control policy
 ###
-### Extract current version from version control if present.  If
-### source is not under source control - take from .version
-### file. Otherwise current version is 0.0
+### Extract current version from version control if present. If source
+### tree is not under source control - take version string from
+### .version file. Otherwise current version is v0.0.0. Both vX.X.X
+### and v.X.X formats are supported.
 
-function (git_version_extract)
-  ### Extract version string
+macro (git_version_extract NAME)
+  ### Extract current version from version control if present.
+  ### If not - take from .version file
   if (EXISTS "${CMAKE_SOURCE_DIR}/.git")
     execute_process (COMMAND git describe
+      WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
       OUTPUT_VARIABLE GIT_TAG_VERSION)
-    if (GIT_TAG_VERSION)
-      file (WRITE .version "${GIT_TAG_VERSION}")
-    endif ()
+    file (WRITE .version "${GIT_TAG_VERSION}")
   else ()
     if (EXISTS "${CMAKE_SOURCE_DIR}/.version")
       execute_process (COMMAND cat .version
+	WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
 	OUTPUT_VARIABLE GIT_TAG_VERSION)
     endif ()
   endif ()
 
   ### Parse version
-  if (NOT GIT_TAG_VERSION)
+  execute_process (COMMAND echo "${GIT_TAG_VERSION}"
+    COMMAND grep -E "^v[0-9][0-9]*\\.[0-9][0-9]*(-.*)?$"
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    OUTPUT_VARIABLE VERSION_TYPE_2)
+  execute_process (COMMAND echo "${GIT_TAG_VERSION}"
+    COMMAND grep -E "^v[0-9][0-9]*\\.[0-9][0-9]*\\.[0-9][0-9]*(-.*)?$"
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    OUTPUT_VARIABLE VERSION_TYPE_3)
+
+  if ("${GIT_TAG_VERSION}" STREQUAL "")
     message (WARNING "No version information was found! "
       "Package should be built from correct source tree.")
-    set (EMBOX_VERSION_MAJOR 0)
-    set (EMBOX_VERSION_MINOR 0)
-    set (EMBOX_VERSION "0.0")
-    set (EMBOX_VERSION_FULL "unknown")
+    set (${NAME}_VERSION_MAJOR 0)
+    set (${NAME}_VERSION_MINOR 0)
+    set (${NAME}_VERSION_PATCH 0)
+    set (${NAME}_VERSION "0.0.0")
+    set (${NAME}_VERSION_FULL "unknown")
   else ()
     execute_process (COMMAND echo "${GIT_TAG_VERSION}"
       COMMAND sed -ne "s/^v\\([[:digit:]]*\\)\\..*/\\1/p"
-      OUTPUT_VARIABLE EMBOX_VERSION_MAJOR
+      OUTPUT_VARIABLE ${NAME}_VERSION_MAJOR
       OUTPUT_STRIP_TRAILING_WHITESPACE)
     execute_process (COMMAND echo "${GIT_TAG_VERSION}"
       COMMAND sed -ne "s/^v[[:digit:]]*\\.\\([[:digit:]]*\\).*/\\1/p"
-      OUTPUT_VARIABLE EMBOX_VERSION_MINOR
+      OUTPUT_VARIABLE ${NAME}_VERSION_MINOR
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process (COMMAND echo "${GIT_TAG_VERSION}"
-      COMMAND sed -ne "s/.*-\\(.*\\)/\\1/p"
-      OUTPUT_VARIABLE EMBOX_VERSION_DIRT
-      OUTPUT_STRIP_TRAILING_WHITESPACE)
-    
-    set (EMBOX_VERSION "${EMBOX_VERSION_MAJOR}.${EMBOX_VERSION_MINOR}")
-    if (EMBOX_VERSION_DIRT)
-      set (EMBOX_VERSION_FULL "${EMBOX_VERSION}-${EMBOX_VERSION_DIRT}")
-    else ()
-      set (EMBOX_VERSION_FULL "${EMBOX_VERSION}")
+    if (VERSION_TYPE_3)
+      execute_process (COMMAND echo "${GIT_TAG_VERSION}"
+	COMMAND sed -ne "s/^v[[:digit:]]*\\.[[:digit:]]*\\.\\([[:digit:]]*\\).*/\\1/p"
+	OUTPUT_VARIABLE ${NAME}_VERSION_PATCH
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
     endif ()
-    
-    message (STATUS "Building version: ${EMBOX_VERSION_FULL}")
+    execute_process (COMMAND echo "${GIT_TAG_VERSION}"
+      COMMAND sed -ne "s/[^-]*-\\(.*\\)/\\1/p"
+      OUTPUT_VARIABLE ${NAME}_VERSION_DIRT
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    if (VERSION_TYPE_3)
+      set (${NAME}_VERSION "${${NAME}_VERSION_MAJOR}.${${NAME}_VERSION_MINOR}.${${NAME}_VERSION_PATCH}")
+    else ()
+      set (${NAME}_VERSION "${${NAME}_VERSION_MAJOR}.${${NAME}_VERSION_MINOR}")
+    endif ()
+
+    if (${NAME}_VERSION_DIRT STREQUAL "")
+      set (${NAME}_VERSION_FULL "${${NAME}_VERSION}")
+    else ()
+      set (${NAME}_VERSION_FULL "${${NAME}_VERSION}-${${NAME}_VERSION_DIRT}")
+    endif ()
   endif ()
-endfunction (git_version_extract)
+
+  message (STATUS "Building version: ${EMBOX_VERSION_FULL}")
+endmacro (git_version_extract)
